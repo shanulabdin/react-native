aad87e8a6f3674b2377d80e572b9f67218248c49
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-26 19:48:50 +0500

    reusing the components.

--- PracticeOne/app/index.tsx
+++ PracticeOne/app/index.tsx
@@ -2,10 +2,22 @@ import { ProfileCard } from "../components/ProfileCard";
 
 export default function Index() {
   return (
-    <ProfileCard
-      name="John Doe"
-      age={30}
-      profession="Software Engineer" 
-    />
+    <>
+      <ProfileCard
+        name="John Doe"
+        age={30}
+        profession="Software Engineer"
+      />
+      <ProfileCard
+        name="Ali"
+        age={22}
+        profession={"Student"}
+      />
+      <ProfileCard
+        name="Sara"
+        age={25}
+        profession={"Developer"}
+      />
+    </>
   );
 }
