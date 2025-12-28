92ef20f42d2278ab868eaee22e2d39748d1d761c
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-28 14:31:10 +0500

    Routing in raect native app using expo router with bottom tab navigation File: PracticeOne/app/_layout.tsx

--- PracticeOne/app/_layout.tsx
+++ PracticeOne/app/_layout.tsx
@@ -1,6 +1,55 @@
-import { Stack } from "expo-router";
-import "./globals.css";
+import { Tabs } from "expo-router";
+import React from "react";
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+const _Layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarItemStyle: {
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarStyle: {
+          backgroundColor: '#0f0d23',
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: 'absolute',
+          overflow: 'hidden',
+          borderWidth: 1,
+          borderColor: '0f0d23',
+        }
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+        }}
+      />
+    </Tabs>
+  );
 }
+
+export default _Layout;
